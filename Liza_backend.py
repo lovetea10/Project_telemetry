b10abcb706a58b9bab4bbf13726168085ceaf1a3
arzaaam <120248747+arzaaam@users.noreply.github.com>
2024-10-10 16:12:07 +0300

    add database reading

--- Liza_backend.py
+++ Liza_backend.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import os
+import sqlite3
+from sqlalchemy import create_engine
+from pymongo import MongoClient
+from typing import Union
 
 
 def read_csv_to_dataframe(file_path: str) -> pd.DataFrame:
     """
     Читает данные из CSV файла и возвращает DataFrame.
+
+    :param file_path: Путь к CSV файлу (строка). Может быть как абсолютным, так и относительным.
+    :return: DataFrame с данными из файла
     """
 
     absolute_path = os.path.abspath(file_path)
@@ -24,10 +31,50 @@ def read_csv_to_dataframe(file_path: str) -> pd.DataFrame:
         raise Exception(f"Произошла ошибка при чтении файла '{absolute_path}': {str(e)}")
 
 
-def filter_dataframe(df: pd.DataFrame, filter_type: str = 'any', columns: list[str] = None,
-                     inplace: bool = False) -> pd.DataFrame | None:
+def get_data_from_db(db_type: str, **kwargs) -> pd.DataFrame:
     """
-        Фильтрует DataFrame по заданным критериям.
+    Получение данных из базы данных.
+    """
+
+    try:
+        if db_type == 'sqlite':
+            conn = sqlite3.connect(kwargs['db_path'])
+            df = pd.read_sql_query(kwargs['query'], conn)
+            conn.close()
+
+        elif db_type == 'postgres':
+            engine = create_engine(
+                f"postgresql://{kwargs['user']}:{kwargs['password']}@{kwargs['host']}:{kwargs['port']}/{kwargs['dbname']}")
+            df = pd.read_sql_query(kwargs['query'], engine)
+
+        elif db_type == 'mysql':
+            engine = create_engine(
+                f"mysql+pymysql://{kwargs['user']}:{kwargs['password']}@{kwargs['host']}:{kwargs['port']}/{kwargs['dbname']}")
+            df = pd.read_sql_query(kwargs['query'], engine)
+
+        elif db_type == 'mongodb':
+            client = MongoClient(kwargs['connection_string'])
+            data = list(client[kwargs['database']][kwargs['collection']].find())
+            df = pd.DataFrame(data)
+
+        else:
+            raise ValueError("Unsupported database type")
+
+        return df
+
+    except sqlite3.Error as e:
+        print(f"SQLite error: {e}")
+    except (ValueError, KeyError) as e:
+        print(f"Value error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return None
+
+
+def filter_dataframe(df: pd.DataFrame, filter_type: str = 'any', columns: list[str] = None, inplace: bool = False) -> Union[pd.DataFrame, None]:
+    """
+    Фильтрует DataFrame по заданным критериям.
     """
     if columns is not None:
         if filter_type == 'any':
@@ -55,7 +102,4 @@ if __name__ == "__main__":
         print(df)
     except Exception as e:
         print(e)
-
-    filtered_df = filter_dataframe(df, filter_type='any', columns=['column1', 'column2'], inplace=False)
-
-    print(filtered_df)
+    print(df)
