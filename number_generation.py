ec3e1779946ad8863d68523cb5deaf7f1756b0f6
Ксения <103182733+Qutespy@users.noreply.github.com>
2024-10-16 19:51:29 +0300

    modified:   number_generation.py

--- number_generation.py
+++ number_generation.py
@@ -4,6 +4,14 @@ import scipy.stats as stats
 
 
 class GeneratingNumbers (object):
+
+    #метод создания массива
+    def __init__(self, distribution_options, amount_of_numbers, parametr1, parametr2):
+        self.distribution_options = distribution_options
+        self.amount_of_numbers = amount_of_numbers
+        self.parametr1 = parametr1
+        self.parametr2 = parametr2
+
     def option_recognition(self, distribution_options, amount_of_numbers, parametr1, parametr2):
         # 1. Генерация чисел по равномерному распределению
         if distribution_options == 1:
@@ -24,32 +32,69 @@ class GeneratingNumbers (object):
     
     def data_save(self, numbers):
         #запись данных в файл, через ; с двумя цифрами после запятой, позже можно будет исправить на n-ое кол-во после запятой
-        np.savetxt("random_data.csv", numbers, delimiter=";", fmt="%d")
+        np.savetxt("random_data_array_1d.csv", numbers, delimiter=";", fmt="%d")
 
     #можем ли мы сделать метод визуализации (?) - однозначно!
     def visualisation(self, numbers):
         plt.subplot(2, 2, 1)
         plt.hist(numbers, bins=30, alpha=0.7, color='blue')
         plt.show()
-    
 
+
+class ArrayGeneration(GeneratingNumbers):
+    def __init__(self, distribution_options, size1, size2, parametr1, parametr2):
+        self.distribution_options = distribution_options
+        self.size1 = size1
+        self.size2 = size2
+        self.parametr1 = parametr1
+        self.parametr2 = parametr2
+
+    #создаю двумерный массив и сохраняю, используя наследственный метод
+    def creating(self, distribution_options, size1, size2, parametr1, parametr2):
+        arraysize = size1*size2
+        array1d = super().option_recognition(distribution_options, arraysize, parametr1, parametr2)
+        array2d = []
+        for i in range(size1):
+            start_index = i * size2
+            end_index = start_index + size2
+            array2d.append(array1d[start_index:end_index])
+        return array2d
+    
+    #сохранение двумерного массива в csv
+    def data_save(self, array2d):
+        np.savetxt("random_data_array_2d.csv", array2d, delimiter=";", fmt="%s")
+    
+#просим на вход вариант распределения, кол-во чисел
 distribution_options = int(input ("select the distribution option, 1 - uniform, 2 - normal, 3 - Pierce's law, 4 - Fisher's law  "))
 
 amount_of_numbers = int(input("set the number of generated numbers  "))
 
-print("write a range of numbers   ")
+print("write parametrs for generating of numbers   ")
 
 #для последних двух действует ограничение: они > 0 
 parametr1 = int(input())
 parametr2 = int(input())
 
-obj = GeneratingNumbers()
+if distribution_options == 3 or distribution_options == 4:
+    if parametr1 > 0 and parametr2 > 0:
+        pass
+    else:
+        print("invalid values, please specify positive values")
+        
+#задание размера массива
+size1 = int(input("введите предполагаемый размер массива    "))
+size2 = int(input())
+
+obj = GeneratingNumbers(distribution_options, amount_of_numbers, parametr1, parametr2)
 
 #сохраним массив для визуализации
-our_data = obj.option_recognition(distribution_options, amount_of_numbers, parametr1, parametr2)
+our_data1d = obj.option_recognition(distribution_options, amount_of_numbers, parametr1, parametr2)
 
 #сохраняем его в csv
-obj.data_save(our_data)
+obj.data_save(our_data1d)
 
 # Визуализация результатов
-obj.visualisation(our_data)
+obj.visualisation(our_data1d)
+
+array2d = ArrayGeneration(distribution_options, size1, size2, parametr1, parametr2)
+our_data2d = array2d.creating(distribution_options, size1, size2, parametr1, parametr2)
